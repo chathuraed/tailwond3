aa2a123c6f6cdcf795fd1eaedd93603fcd24c318
chathura.dev93 <chathura.dev93@gmail.com>
2022-01-22 10:50:42 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center items-center">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button class="bg-cyan-500 shadow-lg shadow-cyan-500/50">
+        Subscribe
+      </button>
+      <button class="bg-blue-500 shadow-lg shadow-blue-500/50">
+        Subscribe
+      </button>
+      <button class="bg-indigo-500 shadow-lg shadow-indigo-500/50">
+        Subscribe
+      </button>
     </div>
   );
 }
